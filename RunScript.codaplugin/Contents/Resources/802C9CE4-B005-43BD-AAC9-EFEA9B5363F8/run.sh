149bf50e1f1d2127feecc4cf71214bf4b0136fca
Joseph Bergantine <jbergantine@gmail.com>
2010-09-04 05:46:16 -0600

    styled output of script with CSS

--- RunScript.codaplugin/Contents/Resources/802C9CE4-B005-43BD-AAC9-EFEA9B5363F8/run.sh
+++ RunScript.codaplugin/Contents/Resources/802C9CE4-B005-43BD-AAC9-EFEA9B5363F8/run.sh
@@ -19,9 +19,13 @@ if [[ "${CODA_FILEPATH}" == *.sh || "${CODA_FILEPATH}" == *.bash ]]; then
 	MODE=bash
 fi
 
+echo "<html><head><style type=\"text/css\">body { font-family: \"Helvetica Neue\", Helvetica; font-size: 24px; background-color: #415d75; color: #d0e6ef; margin: 60px; }</style><title>Run Script</title></head><body>"
+
 if [ -n "$MODE" ]; then
 	# extra redirection and piping to force coda to display stderr
 	$MODE "$CODA_FILEPATH" 2>&1 | tee /dev/null
 else
 	echo "Unknown filetype or file has not been saved. File extension must be .bash, .php, .pl, .py, .rb or .sh."
 fi
+
+echo "</body></html>"
