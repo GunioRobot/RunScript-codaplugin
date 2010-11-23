227fe1a4ec54d0ebd607413d1785c4a79bc612b4
max <max@belletti.net>
2010-11-23 11:30:11 +0100

    [+] full path to programs [+] <BR> at end of rows

--- RunScript.codaplugin/Contents/Resources/802C9CE4-B005-43BD-AAC9-EFEA9B5363F8/run.sh
+++ RunScript.codaplugin/Contents/Resources/802C9CE4-B005-43BD-AAC9-EFEA9B5363F8/run.sh
@@ -1,29 +1,29 @@
 #! /bin/bash
 if [[ "${CODA_FILEPATH}" == *.py ]]; then
-	MODE=python
+	MODE="/usr/bin/env python"
 fi
 
 if [[ "${CODA_FILEPATH}" == *.rb ]]; then
-	MODE=ruby
+	MODE="/usr/bin/env ruby"
 fi
 
 if [[ "${CODA_FILEPATH}" == *.pl ]]; then
-	MODE=perl
+	MODE="/usr/bin/env perl"
 fi
 
 if [[ "${CODA_FILEPATH}" == *.php ]]; then
-	MODE=php
+	MODE="/usr/bin/env php"
 fi
 
 if [[ "${CODA_FILEPATH}" == *.sh || "${CODA_FILEPATH}" == *.bash ]]; then
-	MODE=bash
+	MODE="/usr/bin/env bash"
 fi
 
 echo "<html><head><style type=\"text/css\">body { font-family: \"Helvetica Neue\", Helvetica; font-size: 24px; background-color: #415d75; color: #d0e6ef; margin: 60px; }</style><title>Run Script</title></head><body>"
 
 if [ -n "$MODE" ]; then
 	# extra redirection and piping to force coda to display stderr
-	$MODE "$CODA_FILEPATH" 2>&1 | tee /dev/null
+	$MODE "$CODA_FILEPATH" 2>&1 | sed 's:.*:&<br>:' | tee /dev/null
 else
 	echo "Unknown filetype or file has not been saved. File extension must be .bash, .php, .pl, .py, .rb or .sh."
 fi
